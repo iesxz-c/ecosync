b264348b07db36c56251e113cff75012b9f1ca03
Akash Krishnan <akash.krishnan.046@gmail.com>
2026-02-20 21:07:29 +0530

    Add port  support

--- main.py
+++ main.py
@@ -74,3 +74,7 @@ def predict(req: PredictRequest):
         "green_percent": round(green_percent, 2),
         "appliance_usage": appliance_usage
     }
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
